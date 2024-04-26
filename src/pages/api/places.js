d9f39bcd238fb7abd4915e6c2848981a32f6322f
parlabarbedeMerlin <tristan.tr.renard@gmail.com>
2024-04-26 13:57:29 +0200

    back places

--- src/pages/api/places.js
+++ src/pages/api/places.js
@@ -1,9 +1,16 @@
 import addPlaceController from "@/utils/controllers/addPlaceController"
 import { createRoute } from "@/utils/database/createRoute"
+import PlaceModel from "@/utils/database/models/placeModel"
 
 const handler = createRoute(async (req, res) => {
   if (req.method === "POST") {
     await addPlaceController(req, res)
+  }
+
+  if (req.method === "GET") {
+    await PlaceModel.find({}).then((places) => {
+      res.status(200).json({ places })
+    })
   }
 
   res.status(405).json({ message: "Method not allowed ! Please refer to the documentation for more information.", method: req.method })
